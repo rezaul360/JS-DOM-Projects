40f43d8bded5753c88dcfaef59a9ccfc66a640af
Rezaul360 <rezaulkarim3162@gmail.com>
2022-04-18 10:34:16 +0600

    Complete the project-7

--- Project 7 - Working with Both Hexadecimal and RGB Color Code/app.js
+++ Project 7 - Working with Both Hexadecimal and RGB Color Code/app.js
@@ -4,13 +4,16 @@
  * - Also display the hex code to a disabled input field
  * - Add a button to copy the color code
  * - Add a toast message when copied
+ * - User can type their own hex code too
+ * - show rgb color too, but do not need to edit it
+ * - user can also copy the rgb color code
  */
 
-// Global
+// Steps
 
+// Globals
 let div = null;
 
-// Steps
 window.onload = () => {
   main();
 };
@@ -18,17 +21,22 @@ window.onload = () => {
 function main() {
   const root = document.getElementById("root");
   const output = document.getElementById("output");
+  const output2 = document.getElementById("output2");
   const changeBtn = document.getElementById("change-btn");
   const copyBtn = document.getElementById("copy-btn");
 
   changeBtn.addEventListener("click", function () {
-    const bgColor = generateHexColor();
-    root.style.background = bgColor;
-    output.value = bgColor.substring(1).toUpperCase();
+    const color = generateColorDecimal();
+    const hex = generateHexColor(color);
+    const rgb = generateRGBColor(color);
+    root.style.backgroundColor = hex;
+    output.value = hex.substring(1);
+    output2.value = rgb;
   });
+
   copyBtn.addEventListener("click", function () {
     navigator.clipboard.writeText(`#${output.value}`);
-    if (div != null) {
+    if (div !== null) {
       div.remove();
       div = null;
     }
@@ -38,31 +46,53 @@ function main() {
       alert("Invalid Color Code");
     }
   });
+
   output.addEventListener("keyup", function (e) {
     const color = e.target.value;
     if (color) {
       output.value = color.toUpperCase();
       if (isValidHex(color)) {
-        root.style.background = `#${color}`;
+        root.style.backgroundColor = `#${color}`;
       }
     }
   });
 }
-// Step 2 - Random Color Geanerator Function
 
-function generateHexColor() {
+// function 1 - generate three random decimal number for red, green and blue
+// return as an object
+function generateColorDecimal() {
   const red = Math.floor(Math.random() * 255);
   const green = Math.floor(Math.random() * 255);
   const blue = Math.floor(Math.random() * 255);
 
-  return `#${red.toString(16)}${green.toString(16)}${blue.toString(16)}`;
+  return {
+    red,
+    green,
+    blue,
+  };
+}
+
+// function 2 - generate hex color code
+function generateHexColor({ red, green, blue }) {
+  const getTwoCode = (value) => {
+    const hex = value.toString(16);
+    return hex.length === 1 ? `0${hex}` : hex;
+  };
+
+  return `#${getTwoCode(red)}${getTwoCode(green)}${getTwoCode(
+    blue
+  )}`.toUpperCase();
+}
+
+// function 3 - generate rgba color code
+function generateRGBColor({ red, green, blue }) {
+  return `rgb(${red}, ${green}, ${blue})`;
 }
 
 function generateToastMessage(msg) {
   div = document.createElement("div");
-  div.innerHTML = msg;
+  div.innerText = msg;
   div.className = "toast-message toast-message-slide-in";
-  div.style.background = `#${output.value}`;
 
   div.addEventListener("click", function () {
     div.classList.remove("toast-message-slide-in");
@@ -78,13 +108,16 @@ function generateToastMessage(msg) {
 }
 
 /**
- *
  * @param {string} color : ;
  */
 function isValidHex(color) {
-  if (color.length != 6) return false;
+  if (color.length !== 6) return false;
   return /^[0-9A-Fa-f]{6}$/i.test(color);
 }
+
+// Step 1 - create onload handler
+
+// step 2 - random color generator function
 
 // step 3 - collect all necessary references
 
@@ -97,3 +130,13 @@ function isValidHex(color) {
 // Step 7 - create a dynamic toast message
 
 // step 8 - clear toast message
+
+// step 9 - create isHexValid function
+
+// step 10 - implement change handler on input field
+
+// step 11 - prevent copying hex code if it is not valid
+
+// step 12 - refactor the color generator function
+
+// step 13 - update color code to display rbg colors
