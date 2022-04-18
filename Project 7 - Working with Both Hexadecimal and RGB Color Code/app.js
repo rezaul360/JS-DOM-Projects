bc843a822e53ba25cc09f4e46581b01f5208af18
Rezaul360 <rezaulkarim3162@gmail.com>
2022-04-18 08:55:18 +0600

    Project-7 start

---
+++ Project 7 - Working with Both Hexadecimal and RGB Color Code/app.js
@@ -0,0 +1,99 @@
+/**
+ * Project Requirements:
+ * - Change the background color by generating random hex color by clicking a button
+ * - Also display the hex code to a disabled input field
+ * - Add a button to copy the color code
+ * - Add a toast message when copied
+ */
+
+// Global
+
+let div = null;
+
+// Steps
+window.onload = () => {
+  main();
+};
+
+function main() {
+  const root = document.getElementById("root");
+  const output = document.getElementById("output");
+  const changeBtn = document.getElementById("change-btn");
+  const copyBtn = document.getElementById("copy-btn");
+
+  changeBtn.addEventListener("click", function () {
+    const bgColor = generateHexColor();
+    root.style.background = bgColor;
+    output.value = bgColor.substring(1).toUpperCase();
+  });
+  copyBtn.addEventListener("click", function () {
+    navigator.clipboard.writeText(`#${output.value}`);
+    if (div != null) {
+      div.remove();
+      div = null;
+    }
+    if (isValidHex(output.value)) {
+      generateToastMessage(`#${output.value} copied`);
+    } else {
+      alert("Invalid Color Code");
+    }
+  });
+  output.addEventListener("keyup", function (e) {
+    const color = e.target.value;
+    if (color) {
+      output.value = color.toUpperCase();
+      if (isValidHex(color)) {
+        root.style.background = `#${color}`;
+      }
+    }
+  });
+}
+// Step 2 - Random Color Geanerator Function
+
+function generateHexColor() {
+  const red = Math.floor(Math.random() * 255);
+  const green = Math.floor(Math.random() * 255);
+  const blue = Math.floor(Math.random() * 255);
+
+  return `#${red.toString(16)}${green.toString(16)}${blue.toString(16)}`;
+}
+
+function generateToastMessage(msg) {
+  div = document.createElement("div");
+  div.innerHTML = msg;
+  div.className = "toast-message toast-message-slide-in";
+  div.style.background = `#${output.value}`;
+
+  div.addEventListener("click", function () {
+    div.classList.remove("toast-message-slide-in");
+    div.classList.add("toast-message-slide-out");
+
+    div.addEventListener("animationend", function () {
+      div.remove();
+      div = null;
+    });
+  });
+
+  document.body.appendChild(div);
+}
+
+/**
+ *
+ * @param {string} color : ;
+ */
+function isValidHex(color) {
+  if (color.length != 6) return false;
+  return /^[0-9A-Fa-f]{6}$/i.test(color);
+}
+
+// step 3 - collect all necessary references
+
+// step 4 - handle the change button click event
+
+// step 5 - handle the copy button click event
+
+// Step 6 - activate toast message
+
+// Step 7 - create a dynamic toast message
+
+// step 8 - clear toast message
